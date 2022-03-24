85d22697edbce802b356020eed0574f7a90f0012
PuchToTalk <paulchuparis@gmail.com>
2022-03-24 23:58:49 +0100

    -> Retouche branche Healer

--- src/com/isep/rpg/Healer.java
+++ src/com/isep/rpg/Healer.java
@@ -6,20 +6,21 @@ public class Healer extends SpellCaster{
 }
 
     public Healer() {
-        super(1000, 1000, 500, 500, 500);
+        super(1000, 1000, 500, 100, 50);
+
     }
 
 
         public void attack() {
         Random random = new Random();
-        this.heroDamage = random.nextInt(7) + 1;
+        this.heroDamage = random.nextInt(100) + 1;
         this.manaPoints -= this.manaConsomme;
     }
 
     public void defend() {
         Random random = new Random();
-        int degat = random.nextInt(7);
-        int defence = random.nextInt(7);
+        int degat = random.nextInt(100);
+        int defence = random.nextInt(100);
         if (defence < 5 && degat > this.armor) {
             this.lifePoints += this.armor - degat;
         }
