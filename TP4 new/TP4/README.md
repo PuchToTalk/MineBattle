2102bdf1b4f9cdacaecc0e6a90cb4d692c336ade
PuchToTalk <90144938+PuchToTalk@users.noreply.github.com>
2022-03-30 23:47:12 +0200

    Delete TP4 new/TP4 directory

--- TP4 new/TP4/README.md
+++
@@ -1,8 +0,0 @@
-# TP4_test
-
-13-02-22
-+ brouillons
-
-
-23-03-22
-reprise depuis le début
--- TP4 new/TP4/src/com/isep/rpg/Potion.java
+++
@@ -1,9 +0,0 @@
-package com.isep.rpg;
-
-public class Potion extends Hero implements Consumable{
-    public int potion = 10;
-
-    public Potion() {
-    }
-}
-}
