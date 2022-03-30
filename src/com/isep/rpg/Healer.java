8f4830075c2b45510813eb1fa0c121ee43be158e
PuchToTalk <paulchuparis@gmail.com>
2022-03-30 23:27:08 +0200

    -> Changement pour WARRIOR

--- src/com/isep/rpg/Healer.java
+++ src/com/isep/rpg/Healer.java
@@ -1,26 +1,26 @@
 package com.isep.rpg;
 
+
 import java.util.Random;
 
-public class Healer extends SpellCaster{
-}
-
+public class Healer extends SpellCaster {
     public Healer() {
-        super(1000, 1000, 500, 100, 50);
-
+        this.lifePoints = 10;
+        this.armor = 1;
+        this.manaPoints = 20;
+        this.manaConsomme = 3;
     }
 
-
-        public void attack() {
+    public void attack() {
         Random random = new Random();
-        this.heroDamage = random.nextInt(100) + 1;
+        this.weaponDamage = random.nextInt(7) + 1;
         this.manaPoints -= this.manaConsomme;
     }
 
     public void defend() {
         Random random = new Random();
-        int degat = random.nextInt(100);
-        int defence = random.nextInt(100);
+        int degat = random.nextInt(7);
+        int defence = random.nextInt(7);
         if (defence < 5 && degat > this.armor) {
             this.lifePoints += this.armor - degat;
         }
