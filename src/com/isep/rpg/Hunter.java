b9592e23b9a0201795863f6a3de4895a33eafbba
PuchToTalk <paulchuparis@gmail.com>
2022-03-30 23:25:03 +0200

    -> Changement pour HUNTER

--- src/com/isep/rpg/Hunter.java
+++ src/com/isep/rpg/Hunter.java
@@ -1,26 +1,28 @@
 package com.isep.rpg;
 
-import java.util.List;
+
 import java.util.Random;
 
 public class Hunter extends Hero {
-    public int arrows;
-    public Hunter(){
-        super(1000,1000,500,500,500);
+    protected int arrows;
 
+    public Hunter() {
+        this.lifePoints = 15;
+        this.armor = 3;
+        this.arrows = 30;
     }
 
     public void attack() {
         Random random = new Random();
-        this.heroDamage = random.nextInt(100;
+        this.weaponDamage = random.nextInt(7);
         --this.arrows;
     }
 
     public void defend() {
         Random random = new Random();
-        int degat = random.nextInt(100);
-        int defence = random.nextInt(100);
-        if (defence < 20 && degat > this.armor) {
+        int degat = random.nextInt(7);
+        int defence = random.nextInt(7);
+        if (defence < 5 && degat > this.armor) {
             this.lifePoints += this.armor - degat;
         }
 
@@ -29,4 +31,3 @@ public class Hunter extends Hero {
     public void useConsumable(Consumable consumable) {
     }
 }
-
