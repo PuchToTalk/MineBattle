ef51e63b610a49fa29ba4601ca4af8a1bf1d4357
PuchToTalk <paulchuparis@gmail.com>
2022-04-01 00:22:50 +0200

    -> Test unitaire héro HUNTER

--- src/com/isep/rpg/Hunter.java
+++ src/com/isep/rpg/Hunter.java
@@ -4,24 +4,24 @@ package com.isep.rpg;
 import java.util.Random;
 
 public class Hunter extends Hero {
-    protected int arrows;
+    public int arrows;
 
     public Hunter() {
         this.lifePoints = 15;
-        this.armor = 3;
-        this.arrows = 30;
+        this.armor = 5;
+        this.arrows = 10;
     }
 
     public void attack() {
         Random random = new Random();
-        this.weaponDamage = random.nextInt(7);
+        this.weaponDamage = random.nextInt(10);
         --this.arrows;
     }
 
     public void defend() {
         Random random = new Random();
-        int degat = random.nextInt(7);
-        int defence = random.nextInt(7);
+        int degat = random.nextInt(10);
+        int defence = random.nextInt(10);
         if (defence < 5 && degat > this.armor) {
             this.lifePoints += this.armor - degat;
         }
