f812e3ab8cd729a658e1eadf3f30b749f32f949c
PuchToTalk <paulchuparis@gmail.com>
2022-04-03 13:03:02 +0200

    -> Apport choix du héro lors de Battle

--- src/com/isep/rpg/Game.java
+++ src/com/isep/rpg/Game.java
@@ -17,6 +17,7 @@ public class Game {
 
     public void playGame() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("\n------------------------------------------------------------------\n");
         System.out.println("Choix du nombre de héros :");
         int nombreDeHeros = scanner.nextInt();
         this.heroes = new ArrayList();
@@ -27,77 +28,80 @@ public class Game {
             Scanner scan = new Scanner(System.in);
             String nomDuHero;
             if (k == 1) {
+                System.out.println("\n------------------------------------------------------------------\n");
                 System.out.println("Choix du nom de notre premier héros ?");
                 nomDuHero = scan.nextLine();
 
                 do {
                     do {
+                        System.out.println("\n------------------------------------------------------------------\n");
                         System.out.println("Choix du classe de notre / nos héro(s) ? \n1 : Warrior \n2 : Hunter \n3 : Mage\n4 : Healer");
                         z = scan.nextInt();
                     } while(z < 1);
                 } while(z > 4);
             } else {
+                System.out.println("\n------------------------------------------------------------------\n");
                 System.out.println("Choix du nom de notre " + k + "ème héros :");
                 nomDuHero = scan.nextLine();
 
                 do {
                     do {
+                        System.out.println("\n------------------------------------------------------------------\n");
                         System.out.println("Choix de la classe de ce héro : \n1 : Warrior \n2 : Hunter \n3 : Mage\n4 : Healer");
                         z = scan.nextInt();
                     } while(z < 1);
                 } while(z > 4);
             }
 
-            switch(z) {
-                case 1:
+            switch (z) {
+                case 1 -> {
                     Warrior warrior = new Warrior();
                     warrior.name = nomDuHero;
                     this.heroes.add(warrior);
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     Hunter hunter = new Hunter();
                     hunter.name = nomDuHero;
                     this.heroes.add(hunter);
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     Mage mage = new Mage();
                     mage.name = nomDuHero;
                     this.heroes.add(mage);
-                    break;
-                case 4:
+                }
+                case 4 -> {
                     Healer healer = new Healer();
                     healer.name = nomDuHero;
                     this.heroes.add(healer);
+                }
             }
         }
 
 
-
-
-        Iterator var11 = this.heroes.iterator();
-
-        while(var11.hasNext()) {
-            Hero hero = (Hero)var11.next();
+        for (Hero hero : this.heroes) {
             hero.potions = new ArrayList();
 
-            for(k = 0; k < 5; ++k) {
+            for (k = 0; k < 5; ++k) {
                 hero.givePotion();
             }
 
             hero.lembas = new ArrayList();
 
-            for(k = 0; k < 10; ++k) {
+            for (k = 0; k < 10; ++k) {
                 hero.giveFood();
             }
-
+            System.out.println("\n------------------------------------------------------------------\n");
             System.out.println("Nom du héro : " + hero.name);
             System.out.println(hero.getClass());
             System.out.println("Nombre de HP : " + hero.lifePoints);
             System.out.println("Quantité de potions : " + hero.potions.size());
             System.out.println("Quantité de nourritures : " + hero.lembas.size());
+            System.out.println("\n------------------------------------------------------------------\n");
         }
 
         this.generateCombat(this.heroes.size());
+        this.choixHero();
+        this.choixRecompense();
     }
 
     public void generateCombat(int nombreDeHerosVivant) {
@@ -106,6 +110,7 @@ public class Game {
         int combatAleatoire = random.nextInt(3);
         BasicEnemy enemy;
         if (combatAleatoire == 0) {
+            System.out.println("\n------------------------------------------------------------------\n");
             System.out.println("Un boss apparaît et il est accompagné de ses deux serviteurs !\nDébut du combat");
             Enemy boss = new Boss();
             enemy = new BasicEnemy();
@@ -114,7 +119,7 @@ public class Game {
             this.enemies.add(enemy);
             this.enemies.add(enemy2);
         } else {
-            System.out.println(nombreDeHerosVivant + " monstres vous font face !\nDébut du combat");
+            System.out.println(nombreDeHerosVivant + " monstre(s) vous fait/font face !\nDébut du combat");
 
             for(int i = 0; i < nombreDeHerosVivant; ++i) {
                 enemy = new BasicEnemy();
@@ -123,15 +128,39 @@ public class Game {
         }
         // on get l'iterator à partir de la liste enemies
 
-        Iterator var8 = this.enemies.iterator();
-
         // tant qu'il existe un élément contenu dans la liste, on le fait apparaître
 
-        while(var8.hasNext()) {
-            Enemy enemy1 = (Enemy)var8.next();
+        for (Enemy enemy1 : this.enemies) {
             System.out.println("Un " + enemy1.name + " apparaît");
         }
 
+        /*
+        System.out.println("\n------------------------------------------------------------------\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Que comptez-vous faire? : \n1 : Attaquer \n2 : Se défendre \n3 : Utiliser un consommable \n4 : Fuir");
+        int choixDecision = scanner.nextInt();
+        System.out.println(choixDecision);
+
+    */
+
     }
+
+
+    public void choixHero() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Que comptez-vous faire? : \n1 : Attaquer \n2 : Se défendre \n3 : Utiliser un consommable \n4 : Fuir");
+        int choixDecision = scanner.nextInt();
+    }
+
+
+
+
+
+    public void choixRecompense(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quelle récompense choisir ? : \n1 : Armure ++ \n2 : Arme ++ \n3 : Potion HP++ \n4 : Nourriture HP++ \n5 : Potion Qtité ++ \n6 : Nourriture Qtité ++ \n7 : Flèche Qtité ++ \n8 : Coût mana -- \n9 : Sort ++");
+        int choixRecomp = scanner.nextInt();
+    }
+
 }
 
