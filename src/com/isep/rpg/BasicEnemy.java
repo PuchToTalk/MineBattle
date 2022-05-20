4b37c92869b161c16a36813233f500b6936dd91c
PuchToTalk <paulchuparis@gmail.com>
2022-05-20 12:14:22 +0200

    -> Optimisation (2) BasicEnemy ajout construct Builder pr functions basiques

--- src/com/isep/rpg/BasicEnemy.java
+++ src/com/isep/rpg/BasicEnemy.java
@@ -1,16 +1,47 @@
 package com.isep.rpg;
 import java.util.Random;
-/**
- * @auteur  (Paul)
- * @version (v.o1 - 16/05/2022)
- */
+public class BasicEnemy extends Enemy
+{
+    private BasicEnemy(Builder b){
+        this.lifePoints = b.lifePoints;
+        this.name = b.name;
+        this.damage = b.damage;
+    }
 
 
+    /**
+     * @PS : Class BasicEnemy
+     * Cette classe dispose les attributs qu'elle a obtenu par héritage de sa classe mère (Enemy)
+     * idem attributs que Boss aussi
+     *
+     * @auteur(s)  (Paul)
+     * @version (v.o2 - 20/05/2022)
+     */
 
-public class BasicEnemy extends Enemy {
-    public BasicEnemy() {
-        this.lifePoints = 10;
-        this.name = "Minion";
-        this.weaponDamage = 2;
+
+    public static class Builder{
+
+        public int lifePoints;
+        public String name;
+        public int damage;
+
+        public Builder(int lp){
+            this.lifePoints = lp;
+        }
+
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDamage(int damage){
+            this.damage = damage;
+            return this;
+        }
+
+        public BasicEnemy build(){
+            return new BasicEnemy(this);
+        }
     }
 }
+
