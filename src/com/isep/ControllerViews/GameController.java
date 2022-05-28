96d46b3d5b415f2424037cda70e294e453bf07b6
PuchToTalk <paulchuparis@gmail.com>
2022-05-28 11:33:25 +0200

    -> Interface (2) (v.w2) Validation SceneBuilders (.fxml) Ajout fcts affichage Ajout boutons = relié à actions

--- src/com/isep/ControllerViews/GameController.java
+++ src/com/isep/ControllerViews/GameController.java
@@ -1,7 +1,7 @@
 package com.isep.ControllerViews;
 /**
  * @auteur(s)  (Paul)
- * @version (v.w1 - 25/05/2022)
+ * @version (v.w1 - 26/05/2022)
  * Début Interface
  * Doc FXML quasi OK (cf Scene Builders + croquis)
  * **/
@@ -30,10 +30,18 @@ import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
-/** Attributs **/
-    public VBox heroContainer;
-    public VBox enemiesContainer;
-    private Game game;
+/** Attributs updated : bordure vbox, toutes les model de cases qu'on va utiliser
+ * VBox : essentiellement car mettre en évidence camps HERO // ENEMY
+ * ajout de BorderPane
+ * MsgText : pour afficher en textuel le message de l'action réalisé
+ * **/
+
+public VBox heroContainer; //Attribut contenant données du Hero (fenêtre gauche)
+    public VBox enemiesContainer; //Attribut contenant données du Enemy (fenêtre droite)
+    public BorderPane screenContainer;  //Bordure = la Border Box qui contient élémts du Sidebar
+    public Text messageText; // Attribut qui affiche message de l'action choisi (ex : " attaque réussi / flèche bien ajouté etc.."
+    private Game game; //Attribut "game" contenant l'ens de la GameLogic car s'agit d'une instance de la Game
+    private int heroNumber = 0; // Attribut qui s'active lors du choix de récompenses puis disparaît après  choix des récompenses
 
 
 
@@ -45,7 +53,13 @@ public class GameController implements Initializable {
     }
 
 
-    // fct popup setup
+    /**
+     *
+     *            Grâce à "TextInputDialog" : possibilité d'afficher popup avec comme paramètres :
+     *            Txt @contenu : le texte qu'on mettra (du type "combien de joueurs ? ") => utiliser le même principe pour ricochet
+     *                  @titre : Mini rpg lite
+     */
+
     private String ask(String txt) {
         TextInputDialog tid = new TextInputDialog();
         tid.setHeaderText(txt);
@@ -55,7 +69,12 @@ public class GameController implements Initializable {
         return tid.getResult();
     }
 
-    // capte les ENTIERS qu'a soumis l'user lors du choix du "nombre de héro" de départ
+
+    /**
+     * Faisant appel à ask(txt)
+     * > on obtient le nombre total de héro via le "parseInt" pour chaque classe
+     */
+
 
     private void setGame() {
         int totalHeroes = Integer.parseInt(ask("Sasir Nombre total de Hero(s)"));
@@ -67,7 +86,12 @@ public class GameController implements Initializable {
     }
 
 
-    // paramétrage d'un bouton
+    /**
+     * Nom à afficher sur bouton
+     * path, source de l'image recherché depuis mon répertoire pour l'image du background
+     *
+     * Sortie : Bouton "btn"
+     */
 
     private Button getButton(String name, String path) {
         Button btn = new Button(name);
@@ -76,16 +100,80 @@ public class GameController implements Initializable {
         return btn;
     }
 
-    // nos 4 futurs boutons inch
+
+    /**
+     * @auteur(s)  (Paul)
+     * @version (v.w2 - 27/05/2022)
+     * Interface (in progress)
+     * Scene Builders (cf doc.fxml) : ok <=> equivaut au croquis de départ avec 2 camps </=>
+     * Mettre en place actions dynamiques sur BOUTONS
+     * Msg à afficher de l'action réalisé
+     * Attribuer actions sur chaque boutons
+     * Boutons (aligné)
+     * **/
+
+
+
+
+    /**
+     *  Affiche en Message textuel, quelle fonction / action du héros a été effectuée
+     */
+
+    private void setMessageText(String txt) {
+        messageText.setText(txt);
+    }
+}
+
+
+    /**
+     * Applique la fonction employé correspondant au bouton appuyé
+     * Utilisation d'un objet de Action Event (eventHandlers) pour connaître quelle case/ bouton a été sélectionné par l'user
+     */
+
+    private void actionPerformed(ActionEvent e) {
+        // à compléter
+
+        if (((Button) e.getSource()).getText().equalsIgnoreCase("Attaque")) {
+            setMessageText(game.attack());
+        } else if (((Button) e.getSource()).getText().equalsIgnoreCase("Defense")) {
+            setMessageText(game.defend());
+        } else if (((Button) e.getSource()).getText().equalsIgnoreCase("Utilise food")) {
+            setMessageText(game.consumeFood());
+        } else if (((Button) e.getSource()).getText().equalsIgnoreCase("Utilise potion")) {
+            setMessageText(game.consumePotion());
+        }
+        try {
+        } catch (Exception ignored) {
+            // erreur à prédire : à compléter
+        }
+    }
+
+
+
+    /**
+     * Place les fcts attack(), defend(), useConsumable() (enfin food ou lembas) dans un button spécifique;
+     * Initialise + Attribue action listener
+     * l'ajoute puis afffiche sur l'écran du box milieu
+     */
 
     private void addGameStartButtons() {
 
-        Button btn = getButton("Attaque");
-        Button btn1 = getButton("Defense");
-        Button btn2 = getButton("Utilise food");
-        Button btn3 = getButton("Utilise potion");
+        Button btn = getButton("Attaque", "game/ControllerViews/images/sword.jpg");
+        Button btn1 = getButton("Defense", "game/ControllerViews/images/shield.jpg");
+        Button btn2 = getButton("Utilise food", "game/ControllerViews/images/gapple.jpg");
+        Button btn3 = getButton("Utilise potion", "game/ControllerViews/images/potion.jpg");
 
+        TilePane tp = new TilePane();
+        tp.setAlignment(Pos.CENTER);
+        tp.getChildren().addAll(btn, btn1, btn2, btn3);
+        screenContainer.setCenter(tp);
+        tp.setHgap(10);
+        tp.setVgap(10);
+
+        btn.setOnAction(this::actionPerformed);
+        btn1.setOnAction(this::actionPerformed);
+        btn2.setOnAction(this::actionPerformed);
+        btn3.setOnAction(this::actionPerformed);
     }
-
 
 }
--- src/com/isep/Main.java
+++ src/com/isep/Main.java
@@ -1,21 +1,23 @@
 package com.isep;
 
-import com.isep.rpg.Game;
-
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class Main {
-    public Main() {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ControllerViews/views/MiniRPGLite.fxml"));
+        Scene scene = new Scene(root, 899,583);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Game game = new Game();
+        launch(args);
+    }
 
 }
-// Nouveau main à lier avec la future interface
